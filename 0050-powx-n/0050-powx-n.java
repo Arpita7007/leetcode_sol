7770b1320866dc1aa2d9415e01d90d0e688073ee
Arpita Sharma <arpitasharma8808@gmail.com>
2026-08-11 11:27:48 +0530

    Time: 0 ms (100%), Space: 47.5 MB (85.43%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return solve(x, (long) n);
+    }
+
+    private double solve(double x, long n) {
+        if (n == 0) return 1.0;
+        if (n < 0) return solve(1 / x, -n);
+
+        double half = solve(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+}
